208161be39ce444239082153d3f2d12d980d1a7b
OmerAtayilmaz <omeratayilmaz@gmail.com>
2022-11-07 14:50:05 +0300

    Ghost loader structure learned gracefully

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -9,11 +9,16 @@ import { MovieService } from 'src/app/services/movie.service';
 export class MovieListComponent implements OnInit {
   movieList:any;
   searchText:any;
+  loader:boolean=true;
+  totalCount:number=10;
   constructor(private movieService:MovieService) { }
 
   ngOnInit(): void {
     this.movieService.getMovieList().subscribe(res=>{
-      this.movieList=res
+      setTimeout(()=>{
+        this.movieList=res
+        this.loader=false;
+      },3000);
   });
   }
 
